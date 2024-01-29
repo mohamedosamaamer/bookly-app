e7c6983ee316f9d2d121cc7692330971bde8209e
mohamed <medoosos57@gmail.com>
2024-01-30 00:45:38 +0200

    create splash view body

--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -6,7 +6,8 @@ class SplashView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return  Scaffold(
+      appBar: AppBar(),
       body: SplashViewBody(),
     );
   }
