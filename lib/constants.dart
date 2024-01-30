3228ec62fc8e34c74af88158dbaae20a02c985e6
mohamed <medoosos57@gmail.com>
2024-01-31 01:23:57 +0200

    refactor init state & navigate home

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 const KPrimaryColor =  Color(0xff100b20);
+const KTransitionDuration = Duration(milliseconds: 250);
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,6 +1,9 @@
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/features/home/presentation/views/home_view.dart';
 import 'package:bookly_app/features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SplashViewBody extends StatefulWidget {
   SplashViewBody({super.key});
@@ -18,14 +21,9 @@ class _SplashViewBodyState extends State<SplashViewBody>
   @override
   void initState() {
     super.initState();
+    initSlidingAnimation();
 
-    animationController =
-        AnimationController(vsync: this, duration: const Duration(seconds: 1));
-
-    sliderAnimation = Tween<Offset>(begin: const Offset(0, 5), end: Offset.zero)
-        .animate(animationController);
-
-    animationController.forward();
+    navigateToHome();
   }
 
   @override
@@ -48,5 +46,22 @@ class _SplashViewBodyState extends State<SplashViewBody>
         SlidingText(sliderAnimation: sliderAnimation)
       ],
     );
+  }
+
+  void initSlidingAnimation() {
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+
+    sliderAnimation = Tween<Offset>(begin: const Offset(0, 5), end: Offset.zero)
+        .animate(animationController);
+
+    animationController.forward();
+  }
+
+  void navigateToHome() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Get.to(() => const HomeView(),
+          transition: Transition.fade, duration: KTransitionDuration);
+    });
   }
 }
