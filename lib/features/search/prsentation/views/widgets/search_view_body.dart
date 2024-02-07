9ebf08b2c49b1b0745ae01fcef8cb953f70dde2f
mohamed <medoosos57@gmail.com>
2024-02-08 01:52:17 +0200

    search view done

--- lib/features/search/prsentation/views/widgets/search_view_body.dart
+++ lib/features/search/prsentation/views/widgets/search_view_body.dart
@@ -1,3 +1,6 @@
+import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/best_seller_list_view.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/book_list_view_item.dart';
 import 'package:bookly_app/features/search/prsentation/views/widgets/custom_search_text_field.dart';
 import 'package:flutter/material.dart';
 
@@ -9,8 +12,34 @@ class SearchViewBody extends StatelessWidget {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 30),
       child: Column(
-        children: [CustomSearchTextField()],
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomSearchTextField(),
+          SizedBox(height: 16,),
+          Text('Search Result',style: Styles.textStyle18,),
+          SizedBox(height: 16,),
+          Expanded(child: SearchResultListView()),
+        ],
       ),
     );
+  }
+}
+
+class SearchResultListView extends StatelessWidget {
+  const SearchResultListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      // physics: const NeverScrollableScrollPhysics(),
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return const Padding(
+          padding: EdgeInsets.symmetric(vertical: 10),
+          child: BookListViewItem(),
+        );
+      },
+    );
+    ;
   }
 }
