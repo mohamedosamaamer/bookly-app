a72c27552a4fc19031499f23f7663f3098d4fb84
mohamed <medoosos57@gmail.com>
2024-02-18 01:54:28 +0200

    display feature booka part one

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:bookly_app/constants.dart';
-import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:bookly_app/core/utils/service_locator.dart';
 import 'package:bookly_app/features/home/data/repos/home_repo_impl.dart';
 import 'package:bookly_app/features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart';
 import 'package:bookly_app/features/home/presentation/manger/newset_books_cubit/newset_books_cubit.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -24,7 +22,7 @@ class Bookly extends StatelessWidget {
       providers: [
         BlocProvider(
           create: (context) => FeaturedBooksCubit(
-            getIt.get<HomeRepoImpl>(),
+            getIt.get<HomeRepoImpl>()..fetchFeaturedBooks(),
           ),
         ),
         BlocProvider(
